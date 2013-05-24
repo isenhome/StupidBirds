9b5b3185f62b36bec86a6e4cac850b8e544a6be1
isenhome <isenhome>
2013-05-24 11:18:41 +0800

    test area

--- StupidBirds.PinFang.Controllers/HomeController.cs
+++ StupidBirds.PinFang.Controllers/HomeController.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web.Mvc;
 
 namespace StupidBirds.PinFang.Controllers
 {
-    public class Class1
+    public class HomeController:Controller
     {
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
